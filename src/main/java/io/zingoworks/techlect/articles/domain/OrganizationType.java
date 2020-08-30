77c47bbf324bbfcdb978b432af50558bc95194e6
zingoworks <zingoworks@gmail.com>
2020-08-30 21:53:08 +0900

    Add organization 'Spoqa'

--- src/main/java/io/zingoworks/techlect/articles/domain/OrganizationType.java
+++ src/main/java/io/zingoworks/techlect/articles/domain/OrganizationType.java
@@ -10,7 +10,7 @@ public enum OrganizationType {
     WOOWA_BROS("우아한 형제들", "https://www.woowahan.com/favicon/favicon.ico"),
     KAKAO("카카오", "https://tech.kakao.com/favicon.ico"),
     ZOOM("줌", "https://minihouse.zumst.com/20200622145155/zuminternetcom/images/favicon_161107.ico?t=1595878802000"),
-    CARROT_MARKET("당근마켓", "https://d1unjqcospf8gs.cloudfront.net/favicon.ico");
+    SPOQA("스포카", "https://spoqa.github.io/images/favicon.ico");
 
     private final String title;
     private final String faviconPath;
